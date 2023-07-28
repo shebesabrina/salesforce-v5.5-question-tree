e85dc3b646fd3f50e66f613e2b6840c976b85235
Sabrina Robinson <srobinson@invoca.com>
2023-07-28 16:31:08 -0600

    Implemented finish button to take user back to projects page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/projects", to: "projects#index"
+  get "/projects", controller: "projects", action: :index
   get "/upgrade-salesforce-version", controller: "upgrade_salesforce_version", action: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
